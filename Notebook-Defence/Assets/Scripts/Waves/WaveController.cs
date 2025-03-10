c95171db8e5afb9f2fe53deb413068967a437f20
Urmat Urustemov <urustemovurmatk@gmail.com>
2025-03-10 04:07:57 -0500

    More logic for WaveController

--- Notebook-Defence/Assets/Scripts/Waves/WaveController.cs
+++ Notebook-Defence/Assets/Scripts/Waves/WaveController.cs
@@ -5,16 +5,27 @@ public class WaveController : MonoBehaviour
     public static WaveController Instance;
     [SerializeField] private Spawner enemySpawner;
     [SerializeField] private int aliveEnemies;
+    [SerializeField] private int currentWave;
 
     void Start()
     {
         Instance = this;
+
         enemySpawner.GetComponent<Spawner>();
     }
 
     void Update()
     {
-        
+        if(isAllDead())
+        {
+            //TODO Call to start a new wave
+            /* 
+             * enemySpawner.StartWave(currentWave);
+             * SetCurrentEnemyCounter(enemySpawner.GetWaveEnemyCount());
+             * IncreaseWave();
+             */
+
+        }
     }
 
     private void SetCurrentEnemyCounter(int spawnedEnemies)
@@ -23,11 +34,21 @@ public class WaveController : MonoBehaviour
             aliveEnemies = spawnedEnemies;
     }
 
-    private void DecreaseAliveEnemies()
+    public void DecreaseAliveEnemies()
     {
         if(aliveEnemies > 0)
         {
             aliveEnemies--;
         }
+    }
+
+    private bool isAllDead()
+    {
+        return (aliveEnemies <= 0);
+    }
+
+    private void IncreaseWaveCounter()
+    {
+        currentWave++;
     }
 }
